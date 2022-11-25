df25d5b31e0bba69a176eb9fc042a9e68126cc47
Marc Calvente <marc.calvente.lozano@gmail.com>
2022-11-25 16:18:41 +0100

    25/11 - 16:00

--- prueba_cam.py
+++ prueba_cam.py
@@ -2,10 +2,11 @@ import cv2
 import numpy as np
 
 video = cv2.VideoCapture(0)
-# video = cv2.VideoCapture("./data/banana_horizontal.mp4")
 
-YELLOW_MIN = np.array([15, 70, 70],np.uint8)
-YELLOW_MAX = np.array([35, 255, 255],np.uint8)
+YELLOW_MIN = np.array([15, 70, 40],np.uint8)
+YELLOW_MAX = np.array([35, 255, 125],np.uint8)
+
+# height =  480, width = 640
 
 while True:
     ret, frame = video.read()
@@ -13,17 +14,38 @@ while True:
     hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     frame_threshed = cv2.inRange(hsv_frame, YELLOW_MIN, YELLOW_MAX)
 
-    cv2.imshow("Frame", frame_threshed)
+    contours, _ = cv2.findContours(frame_threshed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    key = cv2.waitKey(1) & 0XFF
+    if len(contours) != 0:
+        max_contour = max(contours, key = cv2.contourArea)
+        max_area =  cv2.contourArea(max_contour)
+        if max_area > 100:
+                cv2.drawContours(frame, max_contour, -1, (0, 255, 0), 2)
+
+                M = cv2.moments(max_contour)
+                cX = int(M["m10"] / M["m00"])
+                cY = int(M["m01"] / M["m00"])
+                print(cX, cY)
+                cv2.circle(frame, (cX, cY), 7, (255, 255, 255), -1)
+
+    # for contour in contours:
+    #     area =  cv2.contourArea(contour)
+    #     if area > 100:
+    #         cv2.drawContours(frame, [contour], -1, (0, 255, 0), 2)
+
+    #         M = cv2.moments(contour)
+    #         cX = int(M["m10"] / M["m00"])
+    #         cY = int(M["m01"] / M["m00"])
+    #         print(cX, cY)
+    #         cv2.circle(frame, (cX, cY), 7, (255, 255, 255), -1)
+ 
+    # cv2.imshow("Frame", frame_threshed)
+    cv2.imshow("Frame", frame)
+
+    key = cv2.waitKey(1)
     if key == ord('q'):
         break
 
-# video.stop()
+# video.stop()  esto para que?
 cv2.destroyAllWindows()
-
-
-
-
-#hsv_img = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
 
