1b7fb5936c8dcefd356b59a30f31b39525443603
Marc Calvente <marc.calvente.lozano@gmail.com>
2022-11-29 18:10:11 +0100

    Remote keyboard started

--- distance_keyboard.py
+++ distance_keyboard.py
@@ -2,14 +2,13 @@ import cv2
 import numpy as np
 # from pynput.keyboard import Key, Controller
 from pynput.mouse import Controller
-# import keyboard
-# import pyautogui
+
 
 video = cv2.VideoCapture(0)
 
 YELLOW_MIN = np.array([15, 70, 115],np.uint8)
 YELLOW_MAX = np.array([35, 255, 255],np.uint8)
-# YELLOW_MAX = np.array([35, 255, 125],np.uint8)  ## Depending on the lighting this works better 
+
 
 width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH ))
 height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT ))
@@ -25,14 +24,47 @@ banana_pos_last_frame = 0
 cY = 0
 cX = 0
 
-def on_press(key):
-    try:
-        print('alphanumeric key {0} pressed'.format(
-            key.char))
-    except AttributeError:
-        print('special key {0} pressed'.format(
-            key))
+x_list = []
+y_list = []
 
+def draw_grid(img, grid_shape, color=(0, 255, 0), thickness=1):
+    h, w, _ = img.shape
+    rows, cols = grid_shape
+    dy, dx = h / rows, w / cols
+
+    # draw vertical lines
+    for x in np.linspace(start=dx, stop=w-dx, num=cols-1):
+        x = int(round(x))
+        cv2.line(img, (x, 0), (x, h), color=color, thickness=thickness)
+
+    # draw horizontal lines
+    for y in np.linspace(start=dy, stop=h-dy, num=rows-1):
+        y = int(round(y))
+        cv2.line(img, (0, y), (w, y), color=color, thickness=thickness)
+
+    return img
+
+def get_lines_position(img, grid_shape):
+    h, w, _ = img.shape
+    rows, cols = grid_shape
+
+    # get x positions
+    for x in np.linspace(start=0, stop=w, num=cols+1):
+        x = int(round(x))
+        x_list.append(x)
+
+    # get y positions
+    for y in np.linspace(start=0, stop=h, num=rows+1):
+        y = int(round(y))
+        y_list.append(y)
+
+    return x_list, y_list
+
+_, frame_for_shape = video.read()
+x_list, y_list = get_lines_position(frame_for_shape, (3,10))
+
+print(f"x_list = {x_list}")
+print(f"y_list = {y_list}")
 
 def banana_position():
 
@@ -51,9 +83,7 @@ def banana_position():
         hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
         frame_threshed = cv2.inRange(hsv_frame, YELLOW_MIN, YELLOW_MAX)
 
-        # cv2.line function takes as x=0, y=0 the upper left corner
-        cv2.line(frame, (0, height_upper), (width, height_upper), (255, 255, 255), 1)
-        cv2.line(frame, (0, height_lower), (width, height_lower), (255, 255, 255), 1)
+        frame = draw_grid(frame, (3,10))
 
         contours, _ = cv2.findContours(frame_threshed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
@@ -66,23 +96,9 @@ def banana_position():
                 M = cv2.moments(max_contour)
                 cX = int(M["m10"] / M["m00"])
                 cY = int(M["m01"] / M["m00"])
-                # print(cX, cY)
                 cv2.circle(frame, (cX, cY), 7, (255, 255, 255), -1)
 
-        # banana_pos is for controlling the 
-        if cY < height_upper:
-            banana_pos = 1
-        elif cY > height_lower:
-            banana_pos = -1
-        else:
-            # mouse.position = (960, 540)
-            banana_pos = 0
-
-        mouse.position = (cX/width*1920, cY/height*1080)
-
-        if banana_pos != banana_pos_last_frame:
-            print(banana_pos)
-            banana_pos_last_frame = banana_pos
+        # mouse.position = (cX/width*1920, cY/height*1080)
 
         cv2.imshow("Frame", frame)
 
@@ -90,7 +106,6 @@ def banana_position():
         if key == ord('q'):
             break
 
-    # video.stop()  esto para que?
     cv2.destroyAllWindows()
 
     return None
